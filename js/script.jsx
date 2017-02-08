54ba28bfe614ce62ad1afa660fa54c2ce0013a6d
Eugene Sia <eugene@eugenesia.co.uk>
2017-02-08 17:42:40 +0000

    Add button onClick

--- js/script.jsx
+++ js/script.jsx
@@ -5,7 +5,7 @@
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
